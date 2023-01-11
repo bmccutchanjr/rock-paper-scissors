98f29d54935bb2474543d2487bfc33ae853801c7
Bill McCutchan <bmccutchanjr@gmail.com>
2023-01-10 22:01:05 -0500

    implement <noscript>

--- www/home.js
+++ www/home.js
@@ -6,6 +6,12 @@ getPlayerList();
 window.addEventListener ("load", event =>
 {	event.preventDefault();
 
+	//	If this id running then the browser has not disabled scripts...remove the noscript and hidden classes
+	//	from <footer> and <main> respectively
+
+	document.getElementsByTagName ("footer")[0].classList.remove ("noscript");
+	document.getElementsByTagName ("main")[0].classList.remove ("hidden");
+
 	splashScreen (0);
 
 	//	Add some event listeners...
