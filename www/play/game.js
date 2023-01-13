ff9c1d000dd578db5053f362de5c2fac29cb2470
Bill McCutchan <bmccutchanjr@gmail.com>
2023-01-12 23:28:16 -0500

    put game elements into game.html and style

--- www/play/game.js
+++ www/play/game.js
@@ -1,6 +1,24 @@
-openWebSocket();
+openWebSocket()
+.then (wss =>
+{
+//	This is where I initiate the game by telling the WebSocket server how the user has decided to play.
+//
+//	get cookie 'opponent' -- that tells the server how to configure...
+//	tell the server
+//	once the server has reponded in the affirmative, change the display attribute of the chat space and
+//	game buttons...the DOM must be loaded, but it probably will be by this time...or I could could set a
+//	flag to show the server is ready to go and call a function to change the display.  I also need to set
+//	a flag in window.eventListener() and call the same function there, once everything is ready to go...
+//	the function only changes the display attribute if both flags are set...
+	getCookie ("opponent");
+	
+	wssFlag = true;
+	showDOMElements();
+})
+.catch (error => { alert (error) } );
 
-alert (window.location);
+let domFlag = false;
+let wssFlag = false;
 
 window.addEventListener ("load", event =>
 {	event.preventDefault();
@@ -11,4 +29,32 @@ window.addEventListener ("load", event =>
 	document.getElementsByTagName ("footer")[0].classList.remove ("noscript");
 	document.getElementsByTagName ("main")[0].classList.remove ("hidden");
 
+
+	domFlag = true;
+	showDOMElements();
 })
+
+function getCookie (name)
+{   //  Parse the cookie string and return the value of the selected cookie
+
+	let value = undefined;
+
+	const cookies = document.cookie.split (";");
+	cookies.forEach (c =>
+	{
+		const cPrime = c.split ("=");
+		if (cPrime[0].trim() == name) value = cPrime[1];
+	})
+
+	return value;
+}
+
+function showDOMElements ()
+{	//	Change the display attribute of the DOM elements only when the DOM is loaded, the WebSocket connection has 
+	//	been opened successfully and the server is configured to play...
+
+	if (domFlag && wssFlag)
+	{
+		//	change the display attributes here...
+	}
+}
--- www/play/websocket.js
+++ www/play/websocket.js
@@ -1,58 +1,63 @@
 //	This module is used to open a WebSocket connection to the server and handle communications.  It uses the browser
 //	implementation of the WebSocket protocol.
 
+const wss = {};
+
 function openWebSocket ()
 {	//  Open a WebSocket to the test server and add some event listeners
 
-	//	In the interest of making this code a little more portable, build the URL for the WebSocket server from
-	//	the URL of the HTTP server this page was loaded from.  Thw HTTP and WebSocket servers are on the same machine
-	//	and listening on the same port.
+	//	Because this is an asynchronous procedure...return a Promise
 
-	const ws = new WebSocket ("ws://" + window.location.host);
+	return new Promise ((resolve, reject) =>
+	{	
+		const wss = new WebSocket ("ws://" + window.location.host);
 
-	ws.onopen = function (event)
-	{
-		ws.send ("Hello!");
-	};
+		//	In the interest of making this code a little more portable, use window.location.host for the URL rather
+		//	than hardcoding it.  The HTTP and WebSocket servers are on the same machine.
 
-	ws.onmessage = function (event)
-	{   //  A message was received from the server
+		wss.onerror = function (event) { reject (error) };
 
-		switch (event.data)
-		{
-			case "ping":
-			{   //  This is a message sent from the server at regular intervals to verify that all of the connections it has opened
-				//  are still active.  If a user closed the web page or navigated away from this page the connection established here
-				//  would no longer be active and if the server didn't clean up the garbage occasionally it would eventiually crash
-				//  and burn.
+		wss.onopen = function (event) { resolve (wss); };
 
-				ws.send ("pong");
-				break;
-			}
+		wss.onmessage = function (event)
+		{   //  A message was received from the server
 
-			case "Welcome aboard!":
-			{   //  This is the server's response to this browser's successful connection.  There really isn't much to do with
-				//  it.  But its not an error either, so accomodate it.
-				break;
-			}
-
-			default:
+			switch (event.data)
 			{
-				const data = JSON.parse(event.data);
+				case "ping":
+				{   //  This is a message sent from the server at regular intervals to verify that all of the connections it has opened
+					//  are still active.  If a user closed the web page or navigated away from this page the connection established here
+					//  would no longer be active and if the server didn't clean up the garbage occasionally it would eventiually crash
+					//  and burn.
 
-				switch (data.message)
+					wss.send ("pong");
+					break;
+				}
+
+				case "Welcome aboard!":
+				{   //  This is the server's response to this browser's successful connection.  There really isn't much to do with
+					//  it.  But its not an error either, so accomodate it.
+					break;
+				}
+
+				default:
 				{
-					case "Availability Change":
+					const data = JSON.parse(event.data);
+
+					switch (data.message)
 					{
-						changeAvailability (data)
-						break;
-					}
-					default:
-					{
-						modal ("WEBSOCKET ERROR:\n\nAn unsupported message was received from the server", buzz);
+						case "Availability Change":
+						{
+							changeAvailability (data)
+							break;
+						}
+						default:
+						{
+							modal ("WEBSOCKET ERROR:\n\nAn unsupported message was received from the server", buzz);
+						}
 					}
 				}
 			}
 		}
-	}
+	})
 }
